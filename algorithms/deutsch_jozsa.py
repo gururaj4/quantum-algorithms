bf7aba846d6a4e883cf53843ee13f2afc2f8e969
gururaj4 <gururaj4@illinois.edu>
2026-06-12 11:29:01 +0530

    feat: implement scalable multi-qubit Deutsch-Jozsa oracles

--- algorithms/deutsch_jozsa.py
+++ algorithms/deutsch_jozsa.py
@@ -1,51 +1,63 @@
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", message=".*urllib3 v2 only supports OpenSSL.*")
-from qiskit.primitives import StatevectorSampler
-from qiskit_aer import AerSimulator
 from qiskit import QuantumCircuit
 import random
+from qiskit_aer import AerSimulator
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
-backend = AerSimulator()
-print(f"backend initialised successfully: {backend}")
+from qiskit.primitives import StatevectorSampler
 
-#gates
-identity = QuantumCircuit(2)
-identity.cx(0,1)
+#designing the oracle
+num_qubits = 3
+num_inputs = 2
+target_qubit = 2
+always_0 = QuantumCircuit(num_qubits)
+always_0_gate = always_0.to_gate()
+always_0_gate.name = "always 0"
+
+always_1 = QuantumCircuit(num_qubits)
+always_1.x(target_qubit)
+always_1_gate = always_1.to_gate()
+always_1_gate.name = "always 1"
+
+identity = QuantumCircuit(num_qubits)
+for i in range(num_inputs):
+    identity.cx(i,target_qubit)
 identity_gate = identity.to_gate()
 identity_gate.name = "identity"
-complement = QuantumCircuit(2)
-complement.x(0)
-complement.cx(0,1)
-complement.x(0)
+
+complement = QuantumCircuit(num_qubits)
+for i in range(num_inputs):
+    complement.x(i)
+    complement.cx(i,target_qubit)
+    complement.x(i)
 complement_gate = complement.to_gate()
 complement_gate.name = "complement"
-always_1 = QuantumCircuit(2)
-always_1.x(1)
-always_1_gate = always_1.to_gate()
-always_1_gate.name = "always_1"
-always_0 = QuantumCircuit(2)
-always_0_gate = always_0.to_gate()
-always_0_gate.name = "always_0"
-#wrapping in the oracle
-oracle_gate = random.choice([identity_gate,complement_gate,always_1_gate,always_0_gate])
-print(f"{oracle_gate.name}")
-#building the circuit
-oracle_test = QuantumCircuit(2,1)
-oracle_test.h(0)
-oracle_test.x(1)
-oracle_test.h(1)
-oracle_test.barrier()
-oracle_test.append(oracle_gate,[0,1])
-oracle_test.barrier()
-oracle_test.h(0)
-oracle_test.measure(0,0)
-oracle_test.draw(output='text')
-#running the circuit
-pm = generate_preset_pass_manager(optimization_level=1, backend= backend)
-isa_circuit = pm.run(oracle_test)
+
+oracle = random.choice([always_0_gate,always_1_gate,identity_gate,complement_gate])
+print(oracle.name)
+
+#building the simulation circuit
+qc = QuantumCircuit(num_qubits,num_inputs)
+for i in range(num_inputs):
+    qc.h(i)
+qc.x(target_qubit)
+qc.h(target_qubit)
+qc.barrier()
+qc.append(oracle,range(num_qubits))
+qc.barrier()
+for i in range(num_inputs):
+    qc.h(i)
+for i in range(num_inputs):
+    qc.measure(i,i)
+qc.draw(output='text')
+
+#running the simulation circuit
+backend = AerSimulator()
+pm = generate_preset_pass_manager(optimization_level=1,backend=backend)
+isa = pm.run(qc)
 sampler = StatevectorSampler()
-job = sampler.run([isa_circuit],shots=1)
-result=job.result()
-data_packet = result[0]
-data_packet.data.c.get_counts()
+job = sampler.run([isa],shots=1000)
+result = job.result()
+counts = result[0].data.c.get_counts()
+print(counts)
