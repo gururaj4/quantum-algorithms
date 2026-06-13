6e4ad18fb8fc0fb1e5e4cc1102c182db807e58d8
gururaj4 <gururaj4@illinois.edu>
2026-06-13 11:13:02 +0530

    feat: finalize fully working deutsch-jozsa engine with integrated aer simulator

--- algorithms/deutsch_jozsa.py
+++ algorithms/deutsch_jozsa.py
@@ -3,61 +3,76 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", message=".*urllib3 v2 only supports OpenSSL.*")
 from qiskit import QuantumCircuit
 import random
-from qiskit_aer import AerSimulator
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit_aer import AerSimulator
 from qiskit.primitives import StatevectorSampler
 
-#designing the oracle
-num_qubits = 3
-num_inputs = 2
-target_qubit = 2
-always_0 = QuantumCircuit(num_qubits)
-always_0_gate = always_0.to_gate()
-always_0_gate.name = "always 0"
+class DeutschJoszaEngine:
+    def __init__(self, num_qubits:int):
+        if num_qubits < 2:
+            raise TypeError("invalid input type, needs to be a natural number greater than 1")
+        self.num_qubits = num_qubits
+        self.input_qubits = num_qubits-1
+        self.target_qubit = self.input_qubits
+        self.qc = None
+        self.oracle = None
+        
+    def _build_initialisation(self):
+        self.qc = QuantumCircuit(self.num_qubits, self.input_qubits)
+        for i in range(self.input_qubits):
+            self.qc.h(i)
+        self.qc.x(self.target_qubit)
+        self.qc.h(self.target_qubit)
+        self.qc.barrier()
+        
+    def _build_measurement(self):
+        for i in range(self.input_qubits):
+            self.qc.h(i)
+            self.qc.measure(i,i)
+            
+    def _oracle_initialisation(self):
+        always_0 = QuantumCircuit(self.num_qubits)
+        always_0_gate = always_0.to_gate()
+        always_0_gate.name = "always 0"
 
-always_1 = QuantumCircuit(num_qubits)
-always_1.x(target_qubit)
-always_1_gate = always_1.to_gate()
-always_1_gate.name = "always 1"
+        always_1 = QuantumCircuit(self.num_qubits)
+        always_1.x(self.target_qubit)
+        always_1_gate = always_1.to_gate()
+        always_1_gate.name = "always 1"
 
-identity = QuantumCircuit(num_qubits)
-for i in range(num_inputs):
-    identity.cx(i,target_qubit)
-identity_gate = identity.to_gate()
-identity_gate.name = "identity"
+        identity = QuantumCircuit(self.num_qubits)
+        for i in range(self.input_qubits):
+            identity.cx(i,self.target_qubit)
+        identity_gate = identity.to_gate()
+        identity_gate.name = "identity"
 
-complement = QuantumCircuit(num_qubits)
-for i in range(num_inputs):
-    complement.x(i)
-    complement.cx(i,target_qubit)
-    complement.x(i)
-complement_gate = complement.to_gate()
-complement_gate.name = "complement"
+        complement = QuantumCircuit(self.num_qubits)
+        for i in range(self.input_qubits):
+            complement.x(i)
+            complement.cx(i,self.target_qubit)
+            complement.x(i)
+        complement_gate = complement.to_gate()
+        complement_gate.name = "complement"
 
-oracle = random.choice([always_0_gate,always_1_gate,identity_gate,complement_gate])
-print(oracle.name)
+        self.oracle = random.choice([always_0_gate,always_1_gate,identity_gate,complement_gate])
+        
+    def construct_circuit(self)->QuantumCircuit:
+        self._build_initialisation()
+        self._oracle_initialisation()
+        self.qc.append(self.oracle,range(self.num_qubits))
+        self.qc.barrier()
+        self._build_measurement()
+        return self.qc
 
-#building the simulation circuit
-qc = QuantumCircuit(num_qubits,num_inputs)
-for i in range(num_inputs):
-    qc.h(i)
-qc.x(target_qubit)
-qc.h(target_qubit)
-qc.barrier()
-qc.append(oracle,range(num_qubits))
-qc.barrier()
-for i in range(num_inputs):
-    qc.h(i)
-for i in range(num_inputs):
-    qc.measure(i,i)
-qc.draw(output='text')
+    def run_simulator(self)-> dict:
+        backend=AerSimulator()
+        pm=generate_preset_pass_manager(optimization_level=1,backend=backend)
+        isa_circuit = pm.run(self.qc)
+        sampler = StatevectorSampler()
+        job = sampler.run([isa_circuit],shots=1)
+        result=job.result()
+        counts = result[0].data.c.get_counts()
+        return counts
 
-#running the simulation circuit
-backend = AerSimulator()
-pm = generate_preset_pass_manager(optimization_level=1,backend=backend)
-isa = pm.run(qc)
-sampler = StatevectorSampler()
-job = sampler.run([isa],shots=1000)
-result = job.result()
-counts = result[0].data.c.get_counts()
-print(counts)
+    def verify(self):
+        print(self.oracle.name)
